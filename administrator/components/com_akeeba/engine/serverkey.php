fba7bacad2cef33c9d38e8ee9249cf7c8cf91163
vargasfabian <vargas.fabian@gmail.com>
2015-10-11 16:26:57 +0000

    change

--- administrator/components/com_akeeba/engine/serverkey.php
+++
@@ -1,1 +0,0 @@
-<?php defined('AKEEBAENGINE') or die(); define('AKEEBA_SERVERKEY', 'MGI5MDk5ZWNhMDZkMzFkMzdlYmQ0NThlODE3YzIzOWM='); ?>
--- administrator/components/com_ckeditor/uninstall_ckeditor.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM #__extensions WHERE element = 'ckeditor' AND name = 'Editor - CKEditor';
--- DELETE FROM   #__components where name='CKEditor Component' AND `option`='com_ckeditor' ;
